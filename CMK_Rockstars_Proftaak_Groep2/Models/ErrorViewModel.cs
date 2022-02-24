e573ec9f36b40a1ced21d914fefc9cb2a3cf79c1
davey van den eijnde <davey2206@LIVE.NL>
2022-02-24 11:56:06 +0100

    Add project files.

---
+++ CMK_Rockstars_Proftaak_Groep2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CMK_Rockstars_Proftaak_Groep2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
