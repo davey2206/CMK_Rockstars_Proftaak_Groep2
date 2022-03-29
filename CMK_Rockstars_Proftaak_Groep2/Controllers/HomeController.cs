331062443662fdb3308c84a727ad946a429125f6
davey van den eijnde <davey2206@LIVE.NL>
2022-03-29 10:38:57 +0200

    added create post fuction and half of edit function

--- CMK_Rockstars_Proftaak_Groep2/Controllers/HomeController.cs
+++ CMK_Rockstars_Proftaak_Groep2/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace CMK_Rockstars_Proftaak_Groep2.Controllers
 {
@@ -38,6 +39,42 @@ namespace CMK_Rockstars_Proftaak_Groep2.Controllers
         public IActionResult Add()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddAsync([Bind("Id,TribeId,RockstarId,Title,Content,TribeName,RockstarName")] Article article)
+        {
+            Article receivedArticle = new Article();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(article), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("https://rockstar-api.azurewebsites.net/api/Article", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    receivedArticle = JsonConvert.DeserializeObject<Article>(apiResponse);
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> EditAsync(string id)
+        {
+            Article article = new Article();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://rockstar-api.azurewebsites.net/api/Article/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    article = JsonConvert.DeserializeObject<Article>(apiResponse);
+                }
+            }
+            ViewBag.article = article;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit([Bind("Id,TribeId,RockstarId,Title,Content,TribeName,RockstarName")] Article article)
+        {
+            return RedirectToAction("Index", "Home");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
