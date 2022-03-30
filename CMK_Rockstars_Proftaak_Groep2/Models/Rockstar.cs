27d4c363923c6d76f4381eddf8eeedf91969f83c
davey van den eijnde <davey2206@LIVE.NL>
2022-03-30 10:00:43 +0200

    can now see rockstars and tribes

--- CMK_Rockstars_Proftaak_Groep2/Models/Rockstar.cs
+++ CMK_Rockstars_Proftaak_Groep2/Models/Rockstar.cs
@@ -4,10 +4,13 @@ namespace CMK_Rockstars_Proftaak_Groep2.Models
 {
     public class Rockstar
     {
-        public int Id { get; set; }
+        [Newtonsoft.Json.JsonProperty("Id", NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string Id { get; set; }
         public string Name { get; set; }
+        [Newtonsoft.Json.JsonProperty("Image", NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public string Image { get; set; }
         public string Description { get; set; }
+        [Newtonsoft.Json.JsonProperty("Role", NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public string Role { get; set; }
     }
 }
