1c0ebaba12553ff9b2f0c252854b5f5bd79e9cb8
davey van den eijnde <davey2206@LIVE.NL>
2022-05-12 13:56:09 +0200

    backend dashboard

--- CMK_Rockstars_Proftaak_Groep2/Controllers/DashboardController.cs
+++ CMK_Rockstars_Proftaak_Groep2/Controllers/DashboardController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CMK_Rockstars_Proftaak_Groep2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace CMK_Rockstars_Proftaak_Groep2.Controllers
@@ -10,7 +13,68 @@ namespace CMK_Rockstars_Proftaak_Groep2.Controllers
     {
         public IActionResult Index()
         {
+            var LastArtikels = GetLastArtikelsAsync();
+            var TopTotalViews = GetTopTotalViewsAsync();
+            var TopViews = GetTopViewsAsync();
+
+            ViewData["LastArtikels"] = LastArtikels;
+            ViewData["TopTotalViews"] = TopTotalViews;
+            ViewData["TopViews"] = TopViews;
             return View();
+        }
+        public async Task<List<Article>> GetLastArtikelsAsync()
+        {
+            List<Article> articleList = new List<Article>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://rockstar-api.azurewebsites.net/api/Article/All"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    articleList = JsonConvert.DeserializeObject<List<Article>>(apiResponse);
+                }
+            }
+            articleList.Sort((x, y) => DateTime.Compare(x.publishDate, y.publishDate));
+            articleList.RemoveRange(0, articleList.Count - 3);
+            articleList.Reverse();
+
+            return articleList;
+        }
+
+        public async Task<List<Article>> GetTopTotalViewsAsync()
+        {
+            List<Article> articleList = new List<Article>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://rockstar-api.azurewebsites.net/api/Article/All"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    articleList = JsonConvert.DeserializeObject<List<Article>>(apiResponse);
+                }
+            }
+
+            articleList = articleList.OrderBy(a => a.totalViewCount).ToList();
+            articleList.RemoveRange(0, articleList.Count - 3);
+            articleList.Reverse();
+
+            return articleList;
+        }
+        public async Task<List<Article>> GetTopViewsAsync()
+        {
+            List<Article> articleList = new List<Article>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://rockstar-api.azurewebsites.net/api/Article/All"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    articleList = JsonConvert.DeserializeObject<List<Article>>(apiResponse);
+                }
+            }
+
+            articleList = articleList.OrderBy(a => a.viewCount).ToList();
+            articleList.RemoveRange(0, articleList.Count - 3);
+            articleList.Reverse();
+
+            return articleList;
         }
     }
 }
