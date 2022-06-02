a778fac9a51981c584ad266b52dcfc6a21634d4e
Loes Reyniers <490511@student.fontys.nl>
2022-06-02 13:19:43 +0200

    to do pagina toegevoegd; stylesheet, controller en index

---
+++ CMK_Rockstars_Proftaak_Groep2/Controllers/ToDoController.cs
@@ -0,0 +1,129 @@
+﻿using CMK_Rockstars_Proftaak_Groep2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CMK_Rockstars_Proftaak_Groep2.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly ILogger<ToDoController> _logger;
+
+        public ToDoController(ILogger<ToDoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Article> articleList = new List<Article>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://rockstar-api.azurewebsites.net/api/Article/All"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    articleList = JsonConvert.DeserializeObject<List<Article>>(apiResponse);
+                }
+            }
+            ViewData["Articles"] = articleList;
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAsync([Bind("Id,TribeId,RockstarId,Title,Content,TribeName,RockstarName,Published")] string submitButton, Article article)
+        {
+            switch (submitButton)
+            {
+                case "Concept":
+                    article.published = false;
+                    break;
+                case "Toevoegen":
+                    article.published = true;
+                    break;
+            }
+
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(article), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("https://rockstar-api.azurewebsites.net/api/Article", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> EditAsync(string id)
+        {
+            Article article = new Article();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://rockstar-api.azurewebsites.net/api/Article/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    article = JsonConvert.DeserializeObject<Article>(apiResponse);
+                }
+            }
+            ViewBag.article = article;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditAsync([Bind("Id,TribeId,RockstarId,Title,Content,TribeName,RockstarName")] string submitButton, Article article)
+        {
+            switch (submitButton)
+            {
+                case "Concept":
+                    article.published = false;
+                    break;
+                case "Bewerkten":
+                    article.published = true;
+                    break;
+            }
+
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(article), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PutAsync("https://rockstar-api.azurewebsites.net/api/Article/" + article.Id, content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteAsync(string Id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.DeleteAsync("https://rockstar-api.azurewebsites.net/api/Article/" + Id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
