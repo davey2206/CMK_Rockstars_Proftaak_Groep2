b6c62ef920a1ddf31da537520c351a0610d8b2a5
Brand, Thijmen T.A <thijmen.brand@student.fontys.nl>
2022-06-14 10:51:28 +0200

    dgij

--- CMK_Rockstars_Proftaak_Groep2/Controllers/DashboardController.cs
+++ CMK_Rockstars_Proftaak_Groep2/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
 ﻿using CMK_Rockstars_Proftaak_Groep2.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
@@ -13,6 +14,15 @@ namespace CMK_Rockstars_Proftaak_Groep2.Controllers
     {
         public async Task<IActionResult> IndexAsync()
         {
+            List<Tribe> userTribeList = new List<Tribe>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://rockstar-api.azurewebsites.net/api/Tribe"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    userTribeList = JsonConvert.DeserializeObject<List<Tribe>>(apiResponse);
+                }
+            }
             List<Article> articleList = new List<Article>();
             using (var httpClient = new HttpClient())
             {
@@ -30,9 +40,23 @@ namespace CMK_Rockstars_Proftaak_Groep2.Controllers
                 ViewData["TopTotalViews"] = GetTopTotalViews(articleList);
                 ViewData["TopViews"] = GetTopViews(articleList);
             }
+            if (userTribeList != null)
+            {
+                ViewData["userTribeList"] = userTribeList;
+            }
+            System.Diagnostics.Debug.WriteLine("submitted");
+
+
             return View();
         }
 
+        [HttpPost]
+        public ActionResult index(Tribe model)
+        {
+            System.Diagnostics.Debug.WriteLine("submitted");
+            ViewData["SelectedTribe"] = model.displayName;
+            return View(model);
+        }
         public List<Article> GetLastArticles(List<Article> articleList)
         {
             List<Article> articles = new List<Article>();
